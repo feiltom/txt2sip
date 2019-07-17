ed2fdde9e659c4975fc2dd459087591dc385b08e
feillant thomas <thomas@feillant.com>
2019-07-17 09:42:14 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:2.7-stretch
+
+RUN apt-get update
+RUN apt-get install -y python-dev gcc make gcc binutils build-essential libasound2-dev jq
+RUN rm -rf /var/lib/apt/lists/*
+
+
+RUN mkdir /work
+WORKDIR /work
+RUN wget http://www.pjsip.org/release/2.6/pjproject-2.6.tar.bz2
+RUN tar -xjf pjproject-2.6.tar.bz2
+RUN cd pjproject-2.6;export CFLAGS="$CFLAGS -fPIC";./configure
+RUN cd pjproject-2.6;export CFLAGS="$CFLAGS -fPIC";make dep
+RUN cd pjproject-2.6;export CFLAGS="$CFLAGS -fPIC";make
+RUN cd pjproject-2.6;cd pjsip-apps/src/python/;python setup.py install
+
+RUN mkdir /data
+COPY test/sdqdqd-qsddqdq /data/sqdqsdqsd-dqdsq-3323
+COPY Script/sipamos.py /work/sipamos.py
+COPY Script/txt2call.sh /work/txt2call.sh
+CMD [ "sh", "/work/txt2call.sh" ]
---
+++ Script/txt2call.sh
@@ -0,0 +1,35 @@
+while [ 1 ]; do
+for mess in /data/*
+ do
+        if [ ! "$mess" = "/data/*" ]
+        then
+        NUMBER=`cat $mess|jq .phone|sed "s/\"//g"`
+        MESSAGE=`cat $mess|jq .message|sed "s/\"//g"`
+
+        curl -H "X-Goog-Api-Key: ${goog_api_key}" \
+          -H "Content-Type: application/json; charset=utf-8" \
+          --data "{
+            'input':{
+              'text':'${MESSAGE}'
+            },
+            'voice':{
+              'languageCode':'fr-fr',
+              'name':'fr-FR-Wavenet-D'
+            },
+            'audioConfig':{
+              'audioEncoding':'LINEAR16'
+            }
+          }" "https://texttospeech.googleapis.com/v1beta1/text:synthesize" > test4.txt
+
+
+        sed 's|audioContent| |' < test4.txt > tmp-output.txt && \
+        tr -d '\n ":{}' < tmp-output.txt > tmp-output-2.txt && \
+        base64 tmp-output-2.txt --decode > message.wav && \
+        rm tmp-output*.txt
+        python2.7 sipamos.py $NUMBER
+        rm $mess
+      fi
+done
+sleep 5
+done
+exit 0
